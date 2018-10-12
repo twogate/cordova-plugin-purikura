55d144d9b2704a1d3324ba618b08a024ba25b99c
keiya <keiya_21@yahoo.co.jp>
2018-10-12 16:32:17 +0900

    Initial commit

---
+++ src/android/PurikuraPlugin.java
@@ -0,0 +1,28 @@
+package com.twogate.plugins.purikura;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class PurikuraPlugin extends CordovaPlugin
+{
+
+	public PurikuraPlugin() {
+		Log.i('PurikuraPlugin', "started")
+	}
+
+	@Override
+	public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException
+	{
+		if(action.equals("sayHello"))
+		{
+			String name = data.getString(0);
+			String message = "Hello World !! Hello, " + name;
+			callbackContext.success(message);
+			return true;
+		}
+		
+		return false;
+	}
+}
+
